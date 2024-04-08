d29a102af0328dd2a519e9a5f1ddd6228bf5b4d8
Narendra21s <sivanarendra098@gmail.com>
2024-04-08 15:31:29 +0530

    added modal from BootaStarp

--- ingex.js
+++ ingex.js
@@ -1,1 +1,45 @@
-console.log("hello world");
+// var store={
+//     taskList:[
+//         {
+//             imageUrl:"",
+//             taskTitle:"",
+//             taskType:"",
+//             taskDescription:""
+//         },
+//         {
+//             imageUrl:"",
+//             taskTitle:"",
+//             taskType:"",
+//             taskDescription:""
+//         },
+//         {
+//             imageUrl:"",
+//             taskTitle:"",
+//             taskType:"",
+//             taskDescription:""
+//         },
+//         {
+//             imageUrl:"",
+//             taskTitle:"",
+//             taskType:"",
+//             taskDescription:""
+//         },
+//         {
+//             imageUrl:"",
+//             taskTitle:"",
+//             taskType:"",
+//             taskDescription:""
+//         }       
+//     ]
+// }
+
+const state={
+    taskList:[],
+};
+
+// DOM operations
+const taskContent =document.querySelector(".task__contents");
+const taskModal=document.querySelector(".task__modal__body");
+
+const htmlTaskContent =({id,title,type,description,url})=>{};
+
