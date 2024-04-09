653bbab8607af9d4e3ba27c11eecda2b1f16b8ea
Narendra21s <sivanarendra098@gmail.com>
2024-04-09 17:49:18 +0530

    comlete the Dropdown and JS

--- ingex.js
+++ ingex.js
@@ -44,7 +44,7 @@ const taskModal=document.querySelector(".task__modal__body");
 // console.log(taskModal);
 
 // Tempalte for the card on screen
-const htmlTaskContent =({id,title,type,description,url})=`
+const htmlTaskContent =({id,title,type,description,url})=>`
    <div class='col-md-6 col-lg-4 mt-3' id=${id} key=${id}>
       <div class='card shadow-sm task__card'>
          <div class='card-header d-flex justify-content-end task__card__header'>
@@ -60,7 +60,7 @@ const htmlTaskContent =({id,title,type,description,url})=`
               url && 
               `<img width='100%' src=${url} alt='Card-Image' class='card-img-top md-3'>`
            }
-          <h4 class='card-title' task__card__title>${title}</h4>
+          <h4 class='card-title task__card__title'>${title}</h4>
         //   class='text-muted' => text color in grey color
           <p calss='card-text trim-3-lines text-muted'>${description}</p>
           <div className="tags text-white d-flex flex-wrap">
@@ -91,14 +91,45 @@ const htmlModalContent =({id,title,description,url})=>{
       </div>
    `
   };
+  
 
+  // We conver JSON to String (i.e is for local storage)
   const updateLocalStorage=()=>{
     localStorage.setItem(
-      "tasky",
+      "task",
       JSON.stringify({
         tasks:state.taskList,
       })
     );
   };
+
+  // where we convert str to JSON (i.e rendering the card in the screen) 
+  const loadIntialData=()=>{
+    const localStorageCopy= JSON.parse(localStorage.task);
+    if(localStorageCopy) state.taskList=localStorageCopy.tasks;
+
+    taskList.map((cardDate)=>{
+        taskContent.insertAdjacentHTML('beforeend',htmlTaskContent(cardDate));
+    });
+  };
+
+  // When we edit .... we need to save
+  const handleSubmit=(event)=>{
+    console.log("event trigger");
+        const id=`${Date.now()}`;
+        const input={
+          url:document.getElementById("imageUrl").value,
+          title:document.getElementById("taskTitle").value,
+          type:document.getElementById("taskType").value,
+          description:document.getElementById("taskDescription").value,
+        };
+        // if(input.title==="" || input.type==="" || input.description===""){
+        //   return alert("Please fill all the necessary fields");
+        // };
+        taskContent.insertAdjacentHTML("beforeend",htmlTaskContent({...input,id}));
+        state.taskList.push({...input,id});
+        updateLocalStorage();
+  }
+
 
   
